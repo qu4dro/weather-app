8f3ad208167e5bc5c8a83ecb935fd8d3143ec4ec
unknown <qu4dro.sl@gmail.com>
2021-11-01 18:11:28 +0800

    Added navigation and menu

--- app/src/main/java/ru/orlovvv/weather/ui/WeatherActivity.kt
+++ app/src/main/java/ru/orlovvv/weather/ui/WeatherActivity.kt
@@ -2,6 +2,9 @@ package ru.orlovvv.weather.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import androidx.navigation.ui.setupWithNavController
 import dagger.hilt.android.AndroidEntryPoint
 import ru.orlovvv.weather.R
 import ru.orlovvv.weather.databinding.ActivityWeatherBinding
@@ -13,15 +16,31 @@ class WeatherActivity : AppCompatActivity() {
     val binding
         get() = _binding!!
 
+    val currentNavigationFragment: Fragment?
+        get() = supportFragmentManager.findFragmentById(R.id.nav_host_fragment)
+            ?.childFragmentManager
+            ?.fragments
+            ?.first()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         _binding = ActivityWeatherBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setNavController()
+    }
+
+    private fun setNavController() {
+        val navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+        binding.bottomNavMenu.apply {
+            setupWithNavController(navController)
+            setOnItemReselectedListener {  }
+        }
     }
 
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
     }
+
 }
